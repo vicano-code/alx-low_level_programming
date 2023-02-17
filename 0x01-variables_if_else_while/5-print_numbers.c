f20a3e3947ff93cdcdc4fc34fb630e398a0c1a19
vicano-code <vc.anokwuru@gmail.com>
2023-02-17 15:12:22 -0800

    C-prog to print mumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num  = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf('\n');
+
+	return (0);
+}

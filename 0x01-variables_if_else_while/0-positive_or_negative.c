d4fc4184902166f4f2068e6727f05ce576dd2f9b
vicano-code <vc.anokwuru@gmail.com>
2023-02-17 06:58:59 -0800

    srand_with_if_else_c-prog

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*This function calculates a random integer and outputs the result*/
+/**
+ * This function calculates a random integer and outputs the result
+ */
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);

129c49c1ce78155daa44f377fac799f0d56b85eb
vicano-code <vc.anokwuru@gmail.com>
2023-02-17 16:07:51 -0800

    C-prog to print hexadecimal in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Print hexadecimal base
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n  = '0';
+	int a_f = 'a'
+
+	while (n  <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (a_f <= 'f')
+	{
+		putchar(a_f);
+		a_f++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

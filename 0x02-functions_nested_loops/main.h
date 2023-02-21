5a0c743063266f42d1e996681edba8c9d7d9f42c
vicano-code <vc.anokwuru@gmail.com>
2023-02-21 14:19:39 -0800

    new commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,5 +24,11 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
+/**
+ * _islower - checks for lowercase
+ * @c: the character to check
+ * Return: int
+ */
 
+int _islower(int c);
 #endif

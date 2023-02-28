c1fafb04ba9c5028e2f03dcc8aff604469066a85
vicano-code <vc.anokwuru@gmail.com>
2023-02-27 17:08:08 -0800

    initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Copy the given string
+ * @dest: Location to copy string to
+ * @src: String to copy
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}

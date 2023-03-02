fef132f7597099aafd0c5541860c3df6d8af09a2
vicano-code <vc.anokwuru@gmail.com>
2023-03-02 03:33:44 -0800

    initial commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements of the array
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int tmp;
+
+	while (i != j)
+	{
+		tmp = *(a + i);
+		*(a + i) = *(a + j);
+		*(a + j) = tmp;
+		i++;
+		j--;
+	}
+}

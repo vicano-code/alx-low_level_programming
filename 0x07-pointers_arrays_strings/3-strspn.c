ab10ad1142b894de37027c5e1315aa1e30129fa8
vicano-code <vc.anokwuru@gmail.com>
2023-03-06 12:20:05 -0800

    function updated

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: number of bytes of substring
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}

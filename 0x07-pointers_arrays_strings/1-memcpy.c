15888c830ccc08d0a82ea9b4f9cf9a39a160f365
vicano-code <vc.anokwuru@gmail.com>
2023-03-06 07:01:23 -0800

    initiall commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory address
+ * @src: source memory address
+ * @n: number of bytes to be copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char **ptr;
+
+	ptr = &dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	_putchar('\n');
+
+	return (*ptr);
+}

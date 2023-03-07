a0cd1928409e677e69056118dbe2ed04e8393a59
vicano-code <vc.anokwuru@gmail.com>
2023-03-07 00:36:43 -0800

    initial commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,5 +10,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	unsigned int i, j, a, highest_index;
+
+	for (i = 0; *(accept + i) != '\0'; i++)
+	{
+		for (j = 0; *(s + j) != '\0'; j++)
+		{
+			if (*(s + j) == *(accept + i))
+				a = j;
+		}
+		if (a > highest_index)
+			highest_index = a;
+	}
+
+	return (highest_index + 1);
 }

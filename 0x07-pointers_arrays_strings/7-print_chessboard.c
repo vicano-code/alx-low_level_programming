efe7001db73023eac2e31600640b837713e59ad9
vicano-code <vc.anokwuru@gmail.com>
2023-03-07 07:05:23 -0800

    initial commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * print_chessboard -  prints the given 2D chessboard array
+ * @a: 2D array
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int j, k;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[j][k]);
+		}
+		_putchar('\n');
+	}
+}

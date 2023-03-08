2ef89d62e82f08264f89738dd254d183340cf532
vicano-code <vc.anokwuru@gmail.com>
2023-03-08 06:13:33 -0800

    initial commit - _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * @x: base value
+ * @y: power value
+ * Return: the result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

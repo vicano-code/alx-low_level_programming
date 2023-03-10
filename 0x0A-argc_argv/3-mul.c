bf07e66f354f40293f488bf2e07f913ea28ac9f0
vicano-code <vc.anokwuru@gmail.com>
2023-03-10 07:53:19 -0800

    added C-prog for 2-number multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 - successful, 1-error
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%i\n", a * b);
+	return (0);
+}

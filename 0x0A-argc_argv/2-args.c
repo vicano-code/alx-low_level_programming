df90df8a8a782d496a2dc0a5763e56375724e236
vicano-code <vc.anokwuru@gmail.com>
2023-03-10 07:16:00 -0800

    added program to print all argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 - successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

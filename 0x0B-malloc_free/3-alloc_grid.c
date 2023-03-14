25d7caa2ced640068864f7ea42e80c005dba852c
vicano-code <vc.anokwuru@gmail.com>
2023-03-14 08:08:49 -0700

    function update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,19 +14,26 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)/* validate input */
 		return (NULL);
 
 	arr = (int **)malloc(height * sizeof(int *));
 	if (arr == NULL)
 		return (NULL);
-	for (i  = 0; i < width; i++)
+	for (i  = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int) * height);
+		arr[i] = (int *)malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
 		}

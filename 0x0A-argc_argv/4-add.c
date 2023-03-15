4c472f50eab6adb79b8b17916a80dabad0b8ef2c
vicano-code <vc.anokwuru@gmail.com>
2023-03-15 01:52:01 -0700

    code correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
+
+/**
+ * _strlen - compute string length
+ * @s: pointer to string
+ * Return: the result
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
 
 /**
  * main - a program that adds positive numbers
@@ -11,7 +28,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	if (argc == 1)
 	{
@@ -22,10 +39,14 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+		/* if (!(*argv[i] >= '0' && *argv[i] <= '9')) */
+		for (j = 0; j < _strlen(argv[i]); j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}

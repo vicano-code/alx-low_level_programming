e11662ac86dbfefbd562c0f4cca444b39f47af28
vicano-code <vc.anokwuru@gmail.com>
2023-03-16 14:16:05 -0700

    function correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,18 +11,19 @@
 int *array_range(int min, int max)
 {
 	int *ptr, i;
+	int n = max - min + 1;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((max - min + 1) * sizeof(ptr));
+	ptr = malloc(sizeof(ptr) * n) ;
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = 0; i < n; i++)
 	{
-		*(ptr + i) = i;
-		i++;
+		*(ptr + i) = min;
+		min++;
 	}
 
 	return (ptr);

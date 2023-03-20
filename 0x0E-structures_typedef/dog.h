25ce886be3f2e45bd42f9a821ac1b1949aef0f22
vicano-code <vc.anokwuru@gmail.com>
2023-03-20 10:16:40 -0700

    include guards/make betty compliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
-struct dog 
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure definition for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif

8c6cd1222279029c628e49d9a3aa3fbf05f7d36b
vicano-code <vc.anokwuru@gmail.com>
2023-03-20 10:43:46 -0700

    function review - add validation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,14 +3,17 @@
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: a variable of struct dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @name: member
+ * @age: member
+ * @owner: member
  * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL) /* validate initialization */
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

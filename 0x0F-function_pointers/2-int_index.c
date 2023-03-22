4484bdb33c8be525f765d56c11fa5497bc994cb3
vicano-code <vc.anokwuru@gmail.com>
2023-03-22 09:38:45 -0700

    correct validation code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	/* validate input */
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	if (array == NULL && cmp == NULL)
-		return (-1);
+
+	/* do search */
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)

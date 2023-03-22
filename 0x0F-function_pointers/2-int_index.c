00a5465dddebb4e63f2eaa9e2bc2e0399f78cc29
vicano-code <vc.anokwuru@gmail.com>
2023-03-22 09:31:14 -0700

    initial commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: the array
+ * @size: number of elements in the array
+ * @cmp: a pointer to the function to be used to compare values
+ * Return: the index of the first element for which
+ * the cmp function does not return 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL && cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+
+}

24220759fe2ec6bffbd9677b7aae4c5bcc4773b2
vicano-code <vc.anokwuru@gmail.com>
2023-03-23 12:29:45 -0700

    initial commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,22 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 #endif

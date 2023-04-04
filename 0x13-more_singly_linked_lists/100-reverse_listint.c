a58b8ea70a54e029d4753b10bb658e91c37a8fd9
vicano-code <vc.anokwuru@gmail.com>
2023-04-04 15:59:58 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint -  reverses a listint_t linked list
+ * @head: pointer to pointer to head node of list
+ * Return: a pointer to the first node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	previous = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+
+	}
+
+	*head = previous;
+	return (*head);
+}

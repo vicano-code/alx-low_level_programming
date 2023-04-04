da2ac97a986e0eca94c9ebe28c77efc2eb63b5e0
vicano-code <vc.anokwuru@gmail.com>
2023-04-04 14:03:10 -0700

    code review

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,8 +46,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (current_node == NULL)
 		{
-			free(new);
-			return (NULL);
+			if (idx == i + 1)
+			{
+				current_node = new;
+				new->next = NULL;
+			}
+			else
+			{
+				free(new);
+				return (NULL);
+			}
 		}
 	}
 	return (new);

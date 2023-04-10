1fcd86c80215181e21d64388f592f866dcd8763e
vicano-code <vc.anokwuru@gmail.com>
2023-04-10 04:08:21 -0700

    return value correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	written_bytes = write(fd, text_content, strlen(text_content));

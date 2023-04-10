72988b20ca4fa8b6f7fb26fab9df10639314a196
vicano-code <vc.anokwuru@gmail.com>
2023-04-10 03:37:08 -0700

    add condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	written_bytes = write(fd, text_content, strlen(text_content));
-	if (written_bytes == -1)
+	if (written_bytes == -1 || written_bytes != strlen(text_content))
 	{
 		close(fd);
 		return (-1);

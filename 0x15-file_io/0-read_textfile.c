2722f907dcc309492226595c86cd70fc2099c9ac
vicano-code <vc.anokwuru@gmail.com>
2023-04-09 18:22:27 -0700

    refactor code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "_putchar.c"
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -14,29 +10,36 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	int i;
-	ssize_t bytes_read;
-	char *ch;
+	int fp; /* file descriptor */
+	ssize_t bytes_read, bytes_write;
+	char *buffer;
 
-	ch = malloc(sizeof(ch) * letters);
 	if (filename == NULL) /*validate filename*/
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
 	fp = open(filename, O_RDONLY);
 	if (fp == -1) /*validate opening file*/
 		return (0);
 
-	bytes_read = read(fp, ch, letters);
-	if(bytes_read == -1)
-		return (0);
-
-	for (i = 0; i < bytes_read; i++)
+	bytes_read = read(fp, buffer, letters);
+	if (bytes_read == -1)
 	{
-		_putchar(ch[i]);
+		free(buffer);
+		close(fp);
+		return (0);
+	}
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_write == -1)
+	{
+		free(buffer);
+		close(fp);
+		return (0);
 	}
 
 	close(fp);
-
 	return (bytes_read);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h> /* ssize_t and size_t */
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* close */
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);

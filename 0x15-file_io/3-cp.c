d79c2b181caf11590dd33b680611bcc4a1515250
vicano-code <vc.anokwuru@gmail.com>
2023-04-10 06:12:15 -0700

    initial commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * copy_file -  copies the content of a file to another file
+ * @file_from: source file
+ * @file_to: destination file
+ * Return: 1-successful
+ */
+
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_read, fd_write, fd_stderr = STDERR_FILENO;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	if (!file_from)
+	{
+		dprintf(fd_stderr, "Error: Can't read from file %s\n ", file_from);
+		exit(98);
+	}
+
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read == -1)
+	{
+		dprintf(fd_stderr, "Error: Can't read from file %s\n ", file_from);
+		exit(98);
+	}
+
+	fd_write = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_write == -1)
+	{
+		dprintf(fd_stderr, "Error: Can't write to %s\n ", file_to);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_read, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_write, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			close(fd_read) == -1 ? dprintf(fd_stderr, "Error: Can't close fd_read %d\n", fd_read) : close(fd_read);
+			close(fd_write) == -1 ? dprintf(fd_stderr, "Error: Can't close fd_write %d\n", fd_write) : close(fd_write);
+		}
+	}
+	close(fd_read) == -1 ? dprintf(fd_stderr, "Error: Can't close fd_read %d\n", fd_read) : close(fd_read);
+	close(fd_write) == -1 ? dprintf(fd_stderr, "Error: Can't close fd_write %d\n", fd_write) : close(fd_write);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: argument array
+ * Return: 0 (success), 97-100 (exit value errors)
+ */
+
+int main(int argc, const char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}

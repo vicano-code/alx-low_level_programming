5152c25fc136e94dec5d58ce770549a8e3cd477a
vicano-code <vc.anokwuru@gmail.com>
2023-04-10 03:31:51 -0700

    initial commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* open */
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
-
-/* close */
 #include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif

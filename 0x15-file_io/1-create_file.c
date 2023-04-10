f1d70917d1f9430faca2c21d610d9bcf10920fa0
vicano-code <vc.anokwuru@gmail.com>
2023-04-10 03:49:38 -0700

    validation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
+
 	written_bytes = write(fd, text_content, strlen(text_content));
-	if (written_bytes == -1 || written_bytes != (ssize_t)strlen(text_content))
+	if (written_bytes == -1)
 	{
 		close(fd);
 		return (-1);

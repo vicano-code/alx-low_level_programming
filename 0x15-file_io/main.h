724a55c32dfb8eca21ca8130c91e02922d803b75
Victor Anokwuru <vc.anokwuru@gmail.com>
2023-08-07 09:46:33 -0700

    first commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

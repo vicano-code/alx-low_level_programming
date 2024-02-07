24a918d35a9735101362364d79a04061e3cdc8c6
Victor <vc.anokwuru@gmail.com>
2024-02-07 07:34:26 -0800

    code review

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,9 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	char *value_dup = NULL;
-	hash_node_t *new_node, *current;
+	char *key_dup;
+	char *value_dup;
+	hash_node_t *new_node;
 
 	if (ht == NULL || (ht->array) == NULL || value ==  NULL)
 		return (0);
@@ -21,28 +22,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* give the index of the key */
 	idx = key_index((const unsigned char *)key, ht->size);
 
+	key_dup = strdup(key);/*key duplicate*/
+	if (!key_dup)
+		return (0);
 	value_dup = strdup(value); /*value duplicate*/
+	if (!value_dup)
+		return (0);
+	/*check and update value of key if exists*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(key_dup);
+		free(value_dup);
 		return (0);
-	new_node->key = (char *)key;
+	}
+	new_node->key = key_dup;
 	new_node->value = value_dup;
 	new_node->next = NULL;
 	if (ht->array[idx] == NULL)
+	{
 		ht->array[idx] = new_node;
+		return (1);
+	}
 	else
-	{ /* check and update value of key if it exists or address collision*/
-		current = ht->array[idx];
-		while (current != NULL)
-		{
-			if (strcmp(current->key, (char *)key) == 0)
-			{
-				current->value = (char *)value;
-				return (1);
-			}
-			current = current->next;
-		}
-		/* collision: add new node at begining of list*/
+	{ /* collision: add new node at begining of list*/
 		new_node->next = ht->array[idx];
 		ht->array[idx] = new_node;
 	}

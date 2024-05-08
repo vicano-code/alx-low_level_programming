e795eb2e720488edec2ad24f84383cf57b973c9e
vicano-code <vc.anokwuru@gmail.com>
2024-05-08 14:14:00 -0700

    some correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -25,16 +25,12 @@ int binary_search(int *array, size_t size, int value)
 			printf("%d, ", array[i]);
 		printf("%lu\n", i);
 		mid = left + (right - left) / 2;
-		if (array[mid] < value)
-		{
-			left = mid + 1;
-		}
-		else if (array[mid] > value)
-		{
-			right = mid - 1;
-		}
-		else
+		if (array[mid] == value)
 			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
 	return (-1);
 }

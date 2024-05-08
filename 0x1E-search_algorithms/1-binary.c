d7de987a16b14d1f19b4b6dd59e695a849ae1811
vicano-code <vc.anokwuru@gmail.com>
2024-05-08 14:28:30 -0700

    validate value

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t right = size - 1;
 	size_t mid;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || !value)
 		return (-1);
 
 	while (left <= right)
